111bd3a015af6891737f10f7ccce62c5f297bf5e
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2018-12-06 21:58:58 +0100

    added getUsersByItem

---
+++ server/src/dataBase.py
@@ -0,0 +1,30 @@
+from user import User
+class Database:
+    """The user class, carry bandages"""
+    # Initializer / Instance attributes
+    cpt=0
+    def __init__(self, name):
+        self.users = {}
+
+    def getUser(self, userNum):
+        return users[usersNum]
+    
+    def addUser(self, name):
+        user= User(cpt,name) 
+        cpt=cpt+1
+        users.append(user)
+
+    def getUsersByItem(self, item):
+        res=[]
+        for user in users:
+            if user.have(item):
+                res.append(user)
+        return res
+
+    
+        
+
+
+
+
+
