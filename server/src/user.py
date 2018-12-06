22680a29977b476824174d51f8049c44e9c417f8
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2018-12-06 21:28:29 +0100

    Add server user class

---
+++ server/src/user.py
@@ -0,0 +1,23 @@
+class User:
+    """The user class, carry bandages"""
+    # Initializer / Instance attributes
+    def __init__(self, name):
+        self.name = name
+        self.lattitude=None
+        self.longitude=None
+        self.inventory = [] 
+
+    def setCoordinate(lat, longi):
+        lattitude=lat
+        longitude=longi
+
+    def addObject(obj):
+        inventory.append(obj)
+
+    
+        
+
+
+
+
+
